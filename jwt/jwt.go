258a96769466ddb3cbf85ed1d802bf273ecda446
Kernelgarden <kernelgarden@gmail.com>
2019-01-09 16:51:48 +0900

    implement jwt

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,1 +1,59 @@
 package jwt
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"strings"
+)
+
+type Header struct {
+	Alg string `json:"alg"`
+	Typ string `json:"typ"`
+}
+
+type Payload struct {
+	Iss    string      	`json:"iss,omitempty"`
+	Exp    int64        `json:"exp,omitempty"`
+	Nbf    int64    	`json:"nbf,omitempty"`
+	Iat    int64        `json:"iat,omitempty"`
+}
+
+func Hmac256(src string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(src))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+func Encode(payload Payload, secret string) (string, error) {
+	header := Header{Alg: "HS256", Typ: "JWT"}
+	str, err := json.Marshal(header)
+	if err != nil {
+		return "", err
+	}
+	encodedHeader := Base64Encode(string(str))
+	serializedPayload, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+	sigVal := encodedHeader + "." + Base64Encode(string(serializedPayload))
+	token := sigVal + "." + Hmac256(sigVal, secret)
+	r := strings.NewReplacer("=", "", "+", "-", "/", "_")
+	return r.Replace(token), nil
+}
+
+func Base64Encode(src string) string {
+	data := []byte(src)
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+func Base64Decode(src string) (string, error) {
+	bytes, err := base64.StdEncoding.DecodeString(src)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -1,1 +1,32 @@
 package jwt
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+var firstEncodeAnswer = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhMzZjMzA0OWIzNjI0OWEzYzlmODg5MWNiMTI3MjQzYyIsImV4cCI6MTQ0MjQzMDA1NCwibmJmIjoxNDQyNDI2NDU0LCJpYXQiOjE0NDI0MjY0NTR9.L1-1VoNu598QvY97rE7mLiSqmJInC9Yb5p-JUvYYUxo"
+var secondEncodeAnswer = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVTEVQNDhTZWtlbE5RMWowOWRrc3dub0MxRWhCY0dEViJ9.mabL6L1hUUtEGX8QLK_L6PiA5HO4b9fabcW1yhVZEB4"
+
+func TestEncode(t *testing.T) {
+	payload := Payload{Iss: "a36c3049b36249a3c9f8891cb127243c", Exp: 1442430054, Nbf: 1442426454, Iat: 1442426454}
+	encoded, err := Encode(payload, "b0970f7fc9564e65xklfn48930b5d08b1")
+	if err != nil {
+		t.Error(fmt.Sprintf("Encode occurs error: %s", err))
+	}
+
+	if strings.Compare(encoded, firstEncodeAnswer) != 0 {
+		t.Error(fmt.Sprintf("Enocde is wrong: %s, answer: %s", encoded, firstEncodeAnswer))
+	}
+
+	payload2 := Payload{Iss: "ULEP48SekelNQ1j09dkswnoC1EhBcGDV"}
+	encoded, err = Encode(payload2, "6gOdOte0AYArWRUO7Zds62i3LDMVMweZ")
+	if err != nil {
+		t.Error(fmt.Sprintf("Encode occurs error: %s", err))
+	}
+
+	if strings.Compare(encoded, secondEncodeAnswer) != 0 {
+		t.Error(fmt.Sprintf("Encode occurs error: %s", err))
+	}
+}
