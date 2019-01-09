f3c167b5b43189d81063d1e83d86de683c118d10
Kernelgarden <kernelgarden@gmail.com>
2019-01-09 16:45:30 +0900

    inplement hash

--- hash/hash.go
+++ hash/hash.go
@@ -1,1 +1,15 @@
 package hash
+
+import "golang.org/x/crypto/bcrypt"
+
+var cost = 12
+
+func EncryptPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	return string(bytes), err
+}
+
+func ValidatePassword(hashedPassword string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
--- hash/hash_test.go
+++ hash/hash_test.go
@@ -1,1 +1,30 @@
 package hash
+
+import "testing"
+
+func TestEncryptPassword(t *testing.T) {
+	pw := "password"
+	hash, err := EncryptPassword(pw)
+	if err != nil {
+		t.Error("HashPassword occurs error!")
+	}
+
+	if pw == hash {
+		t.Error("Not changed before hash and after hash!")
+	}
+}
+
+func TestValidatePassword(t *testing.T) {
+	pw := "password"
+	hash, _ := EncryptPassword(pw)
+
+	isValiated := ValidatePassword(hash, pw)
+	if isValiated != true {
+		t.Error("ValidatePassword has some error!")
+	}
+
+	isValiated = ValidatePassword(hash, pw + "2")
+	if isValiated == true {
+		t.Error("ValidatePasword has some error!")
+	}
+}
